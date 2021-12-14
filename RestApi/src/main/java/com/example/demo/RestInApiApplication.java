edc95ff43c37247d07fd79c8da78bf87f82aea43
Keagan Dodsworth <keagan.dods@gmail.com>
2021-12-14 12:42:27 +0200

    Spring

---
+++ RestApi/src/main/java/com/example/demo/RestInApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestInApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestInApiApplication.class, args);
+	}
+
+}
